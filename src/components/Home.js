78591832baef5dad73914e087929f69989e912d1
Yash Jaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-01-29 18:34:26 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 const Home = () => {
-
+    const navigate = useNavigate()
     return (
         <div id='home-page'>
 
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
+            <button id="home-to-blog" onClick={() => navigate('/blog')}> Go to Blog</button>
+            <button id="home-to-info" onClick={() => navigate('/info')}> Go to Info</button>
 
         </div>
     )
