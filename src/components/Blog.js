155d3b6289600eac819459aaa9401894f3f3f0dd
Yash Jaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-01-29 18:33:40 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
 const Blog = () => {
-
+    const navigate = useNavigate()
 
     return (
         <div id="blog-page">
@@ -10,8 +11,8 @@ const Blog = () => {
             <h2>Checkout my awesome blogs </h2>
             <h3><a href='https://www.newtonschool.co/post/top-13-web-developer-interview-questions'>Web Development Interview questions</a></h3>
             <h3><a href='https://www.newtonschool.co/post/graph-data-structure-explained-with-examples'>Graph Data Structures</a></h3>
-            <button id="blog-to-info"> Go to Info</button>
-            <button id="blog-to-home"> Go to Home</button>
+            <button id="blog-to-info" onClick={() => navigate('/info')}> Go to Info</button>
+            <button id="blog-to-home" onClick={() => navigate('/')}> Go to Home</button>
         </div>
     )
 }
